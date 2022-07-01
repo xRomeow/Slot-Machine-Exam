df6d17e25c0c84f277bc8e6fc9aaae1aaa051141
sagalajerome <jerome.sagala22@gmail.com>
2022-07-01 08:09:22 +0800

    Added Bet function
    Added Bet function

--- Assets/Scripts/EventBroadcaster/EventNames.cs
+++ Assets/Scripts/EventBroadcaster/EventNames.cs
@@ -2,6 +2,7 @@ public class EventNames
 {
     public const string ON_SPIN_CLICKED = "ON_SPIN_CLICKED";
     public const string ON_BET_MODIFIED = "ON_BET_MODIFIED";
+    public const string ON_CASH_DEDUCTED = "ON_CASH_DEDUCTED";
 }
 
 public class Payouts
--- Assets/Scripts/UI/Bet.cs
+++ Assets/Scripts/UI/Bet.cs
@@ -6,62 +6,43 @@ using UnityEngine.UI;
 
 public class Bet : MonoBehaviour
 {
-    //[SerializeField] private Text betText; // convert bet text to int
-    //[SerializeField] private Button increase; // decrease by 10
-    //[SerializeField] private Button decrease; // decrease by 10
-    //private int betCash; // Cash - bet
+    [SerializeField] private Text betText; // convert bet text to int
+    [SerializeField] private Button increase; // decrease by 10
+    [SerializeField] private Button decrease; // decrease by 10
+    private int betCash; // Cash - bet
 
-    //public int playerCash;
+    public int playerCash;
 
-    //void Start()
-    //{
-    //    betCash = 10;
-    //    betText.text = betCash.ToString();
+    void Start()
+    {
+        betCash = 10;
+        betText.text = betCash.ToString();
+        //EventBroadcaster.Instance.AddObserver(EventNames.ON_CASH_DEDUCTED, ModifyBetIncrease);
+    }
 
-        
-    //}
+    public void ModifyBetDecrease()
+    {
+        betCash -= 10;
+        betText.text = betCash.ToString();
 
-    //void Update()
-    //{
-    //    Parameters param = new Parameters();
-    //    playerCash = param.GetParameter<int>("currCashValue", 0);
-    //}
+        if (betCash <= 0)
+        {
+            this.decrease.enabled = false;
+        } else {
+            this.decrease.enabled = true;
+        }
+        Parameters param = new Parameters();
+        param.AddParameter<int>("currBetValue", +10);
+        EventBroadcaster.Instance.PostEvent(EventNames.ON_BET_MODIFIED, param);
 
-    //public void ModifyBetDecrease()
-    //{
-    //    betCash -= 10;
-    //    betText.text = betCash.ToString();
+    }
 
-    //    if (betCash <= 0)
-    //    {
-    //        this.decrease.enabled = false;
-    //    } else {
-    //        this.decrease.enabled = true;
-    //    }
-    //    Parameters param = new Parameters();
-    //    param.AddParameter<int>("currBetValue", +10);
-    //    EventBroadcaster.Instance.PostEvent(EventNames.ON_BET_MODIFIED, param);
-
-    //}
-
-    //public void ModifyBetIncrease()
-    //{
-    //    betCash += 10;
-    //    betText.text = betCash.ToString();
-
-
-    //    if (playerCash == 0)
-    //    {
-    //        this.increase.enabled = false;
-    //    } else
-    //    {
-    //        this.increase.enabled = true;
-    //    }
-    //    Parameters param = new Parameters();
-    //    param.AddParameter<int>("currBetValue", -10);
-    //    EventBroadcaster.Instance.PostEvent(EventNames.ON_BET_MODIFIED, param);
-
-    //}
-
-
+    public void ModifyBetIncrease()
+    {
+        betCash += 10;
+        betText.text = betCash.ToString();
+        Parameters param = new Parameters();
+        param.AddParameter<int>("currBetValue", -10);
+        EventBroadcaster.Instance.PostEvent(EventNames.ON_BET_MODIFIED, param);
+    }
 }
